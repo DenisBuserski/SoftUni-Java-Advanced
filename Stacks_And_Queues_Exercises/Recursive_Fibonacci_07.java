fb79c740b7239d8c9cba07465d2a3656a0036e07
Denis Buserski <denis.buserski@gmail.com>
2022-07-11 06:51:54 +0300

    Update Recursive_Fibonacci_07.java

--- Stacks_And_Queues_Exercises/Recursive_Fibonacci_07.java
+++ Stacks_And_Queues_Exercises/Recursive_Fibonacci_07.java
@@ -1,9 +1,8 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Stacks_And_Queues_Exercises;
+package Stacks_And_Queues_Exercises;
 
 import java.util.Scanner;
 
 public class Recursive_Fibonacci_07 {
-
     public static long [] memoryForFib;
 
     public static void main(String[] args) {
@@ -12,19 +11,16 @@ public class Recursive_Fibonacci_07 {
         int n = Integer.parseInt(scanner.nextLine());
         memoryForFib = new long [n + 1];
         System.out.println(fib(n));
-
-
     }
 
     public static long fib (int n) {
         if (n <= 1) {
             return 1;
         }
-
         if (memoryForFib[n] != 0) {
             return memoryForFib[n];
         }
-
         return memoryForFib[n] = fib(n - 1) + fib(n - 2);
     }
+    
 }
