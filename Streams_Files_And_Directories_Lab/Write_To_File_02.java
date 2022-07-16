77808b7e69f94e3efee313db359401c607c09131
Denis Buserski <denis.buserski@gmail.com>
2022-07-16 19:22:20 +0300

    Update Write_To_File_02.java

--- Streams_Files_And_Directories_Lab/Write_To_File_02.java
+++ Streams_Files_And_Directories_Lab/Write_To_File_02.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Streams_Files_And_Directories_Lab;
+package Streams_Files_And_Directories_Lab;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Write_To_File_02 {
     public static void main(String[] args) throws IOException {
 
-        String path = "C:\\Users\\Acer\\Desktop\\SoftUni\\Advanced - Java\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\JavaOOP.input.txt";
+        String path = "input.txt";
 
         FileInputStream inputStream = new FileInputStream(path);
 
@@ -20,17 +20,12 @@ public class Write_To_File_02 {
 
         while (nextByte != -1) {
             char symbol = (char) nextByte;
-
             if (!separators.contains(symbol)) {
                 printStream.print(symbol);
             }
 
             nextByte = inputStream.read();
         }
-
-
-
-
 
     }
 }
