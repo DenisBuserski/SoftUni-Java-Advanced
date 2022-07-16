0e6b1da7fff2438040c6728fe341e0db339d864c
Denis Buserski <denis.buserski@gmail.com>
2022-07-16 19:13:20 +0300

    Update List_Files_07.java

--- Streams_Files_And_Directories_Lab/List_Files_07.java
+++ Streams_Files_And_Directories_Lab/List_Files_07.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Streams_Files_And_Directories_Lab;
+package Streams_Files_And_Directories_Lab;
 
 import java.io.File;
 import java.io.IOException;
@@ -6,19 +6,15 @@ import java.io.IOException;
 public class List_Files_07 {
     public static void main(String[] args) throws IOException {
 
-        File file =
-                new File("C:\\Users\\Acer\\Desktop\\SoftUni\\Advanced - Java\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams");
+        File file = new File("Files-and-Streams");
 
         File [] files  = file.listFiles();
 
         for (File f : files) {
-
             if (!f.isDirectory()) {
                 System.out.printf("%s: [%d]%n", f.getName(), f.length());
             }
         }
-
-
 
     }
 }
