a646ad259a886adfc3aa65db8e04a62c772f3366
Denis Buserski <denis.buserski@gmail.com>
2022-07-16 19:07:13 +0300

    Update Unique_Usernames_01.java

--- Sets_And_Maps_Advanced_Exercises/Unique_Usernames_01.java
+++ Sets_And_Maps_Advanced_Exercises/Unique_Usernames_01.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Sets_And_Maps_Advanced_Exercises;
+package Sets_And_Maps_Advanced_Exercises;
 
 import java.util.*;
 
@@ -7,13 +7,11 @@ public class Unique_Usernames_01 {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         Set<String> word = new LinkedHashSet<>();
 
         while (n-- > 0) {
-        String input = scanner.nextLine();
-        word.add(input);
-
+            String input = scanner.nextLine();
+            word.add(input);
         }
 
         for (String a : word) {
