ca00ada63846520eec78670b1b8a9b6f7288f351
Denis Buserski <denis.buserski@gmail.com>
2022-07-16 19:19:07 +0300

    Update Read_File_01.java

--- Streams_Files_And_Directories_Lab/Read_File_01.java
+++ Streams_Files_And_Directories_Lab/Read_File_01.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Streams_Files_And_Directories_Lab;
+package Streams_Files_And_Directories_Lab;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -6,16 +6,16 @@ import java.util.Scanner;
 
 public class Read_File_01 {
     public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\Acer\\Desktop\\SoftUni\\Advanced - Java\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\JavaOOP.input.txt";
+        String path = "input.txt";
 
         FileInputStream inputStream = new FileInputStream(path);
         Scanner scanner = new Scanner(inputStream);
 
         int nextByte = inputStream.read();
-
         while (nextByte != -1) {
             System.out.print(Integer.toBinaryString(nextByte) + " ");
             nextByte = inputStream.read();
         }
+        
     }
 }
