83efdd106685969f412d75164abd1197a1800dc0
Denis Buserski <denis.buserski@gmail.com>
2022-07-16 19:14:47 +0300

    Update Nested_Folders_08.java

--- Streams_Files_And_Directories_Lab/Nested_Folders_08.java
+++ Streams_Files_And_Directories_Lab/Nested_Folders_08.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Streams_Files_And_Directories_Lab;
+package Streams_Files_And_Directories_Lab;
 
 import java.io.File;
 import java.io.IOException;
@@ -8,8 +8,7 @@ import java.util.ArrayDeque;
 public class Nested_Folders_08 {
     public static void main(String[] args) throws IOException {
 
-        File file =
-                new File("C:\\Users\\Acer\\Desktop\\SoftUni\\Advanced - Java\\04. Java-Advanced-Files-and-Streams-Lab-Resources");
+        File file = new File(" ");
 
         ArrayDeque<File> files = new ArrayDeque<>();
 
@@ -18,12 +17,10 @@ public class Nested_Folders_08 {
         PrintStream printStream = new PrintStream("nested-out.txt");
 
         while (!files.isEmpty()) {
-
             File innerFile = files.poll();
             printStream.println(innerFile.getName());
 
             for (File f : innerFile.listFiles()) {
-
                 if (f.isDirectory()) {
                     files.offer(f);
                 } else {
@@ -31,8 +28,6 @@ public class Nested_Folders_08 {
                 }
             }
         }
-
-
 
     }
 }
