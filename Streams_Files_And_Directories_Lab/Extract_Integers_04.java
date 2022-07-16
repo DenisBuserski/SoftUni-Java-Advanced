15289d73836b41ce1af67273b24f7e18962871f2
Denis Buserski <denis.buserski@gmail.com>
2022-07-16 19:12:32 +0300

    Update Extract_Integers_04.java

--- Streams_Files_And_Directories_Lab/Extract_Integers_04.java
+++ Streams_Files_And_Directories_Lab/Extract_Integers_04.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Streams_Files_And_Directories_Lab;
+package Streams_Files_And_Directories_Lab;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Extract_Integers_04 {
     public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\Acer\\Desktop\\SoftUni\\Advanced - Java\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\JavaOOP.input.txt";
+        String path = "input.txt";
 
         FileReader reader = new FileReader(path);
 
@@ -18,5 +18,6 @@ public class Extract_Integers_04 {
             }
             scanner.next();
         }
+        
     }
 }
