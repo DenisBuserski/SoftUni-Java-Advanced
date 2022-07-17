35372191e49a08cc13fae624406b99ccecbceb35
Denis Buserski <denis.buserski@gmail.com>
2022-07-17 21:48:13 +0300

    Update Line_Numbers_05.java

--- Streams_Files_And_Directories_Exercises/Line_Numbers_05.java
+++ Streams_Files_And_Directories_Exercises/Line_Numbers_05.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Streams_Files_And_Directories_Exercises;
+package Streams_Files_And_Directories_Exercises;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,15 +9,13 @@ public class Line_Numbers_05 {
     public static int countRow = 1;
     public static void main(String[] args) throws IOException {
 
-        String path = "C:\\Users\\Acer\\Desktop\\SoftUni\\Advanced - Java\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputLineNumbers.txt";
+        String path = "inputLineNumbers.txt";
         String outputPath = "outputLine.txt";
         PrintWriter writer = new PrintWriter(outputPath);
 
         Files.readAllLines(Path.of(path))
-                .forEach(line ->
-                        writer.println(countRow++ + ". " + line));
+                .forEach(line -> writer.println(countRow++ + ". " + line));
 
         writer.close();
-
     }
 }
