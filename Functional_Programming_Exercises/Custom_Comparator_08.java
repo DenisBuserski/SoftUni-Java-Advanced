d8019e9133a221432ca18afbe86201e6d887aa29
Denis Buserski <denis.buserski@gmail.com>
2022-07-18 06:50:33 +0300

    Update Custom_Comparator_08.java

--- Functional_Programming_Exercises/Custom_Comparator_08.java
+++ Functional_Programming_Exercises/Custom_Comparator_08.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Functional_Programming_Exercises;
+package Functional_Programming_Exercises;
 
 import java.util.*;
 import java.util.stream.Collectors;
