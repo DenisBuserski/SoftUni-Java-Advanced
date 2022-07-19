2b5739ca4e36135790bb40e20e0d38ff01cd5651
Denis Buserski <denis.buserski@gmail.com>
2022-07-19 22:44:14 +0300

    Update BankAccount.java

--- Defining_Classes_Lab/Bank_Account_03/BankAccount.java
+++ Defining_Classes_Lab/Bank_Account_03/BankAccount.java
@@ -1,12 +1,10 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Defining_Classes_Lab.Bank_Account_03;
+package Defining_Classes_Lab.Bank_Account_03;
 
 public class BankAccount {
     private static int accountNextId = 1;
     private static double interestRate = 0.02;
-
     private int id;
     private double balance;
-
 
     public BankAccount() {
         this.id = accountNextId;
@@ -28,4 +26,5 @@ public class BankAccount {
     public int getId() {
         return id;
     }
+    
 }
