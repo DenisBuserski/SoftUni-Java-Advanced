9af93c3a94198042a14c8da8fe69ca1113a12dfd
Denis Buserski <denis.buserski@gmail.com>
2022-07-20 07:41:37 +0300

    Update Car.java

--- Defining_Classes_Lab/Constructors_02/Car.java
+++ Defining_Classes_Lab/Constructors_02/Car.java
@@ -1,7 +1,6 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Defining_Classes_Lab.Constructors_02;
+package Defining_Classes_Lab.Constructors_02;
 
 public class Car {
-
     private String brand;
     private String model;
     private int horsePower;
@@ -9,9 +8,11 @@ public class Car {
     public Car(String brand) {
         this(brand, "unknown", -1);
     }
+    
     public Car(String brand, String model) {
         this(brand, model, -1);
     }
+    
     public Car(String brand, int horsePower) {
         this(brand, "unknown", horsePower);
     }
@@ -46,14 +47,11 @@ public class Car {
         this.horsePower = horsePower;
     }
 
-
     public String getInfo() {
         return toString();
     }
 
-
     public boolean equals(Car other) {
-
         return this.brand.equals(other.brand)
                 && this.model.equals(other.model)
                 && this.horsePower == other.horsePower;
@@ -61,8 +59,7 @@ public class Car {
 
     @Override
     public String toString() {
-        return String.format("The car is: %s %s - %d HP.",
-                this.getBrand(), this.getModel(), this.getHorsePower());
+        return String.format("The car is: %s %s - %d HP.", this.getBrand(), this.getModel(), this.getHorsePower());
     }
+    
 }
-
