d10ea0dbb5e9a6166021c6c90eeb71c2e9798c4a
Denis Buserski <denis.buserski@gmail.com>
2022-07-21 08:25:43 +0300

    Update Engine.java

--- Defining_Classes_Exercises/Raw_Data_04/Engine.java
+++ Defining_Classes_Exercises/Raw_Data_04/Engine.java
@@ -1,7 +1,6 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Defining_Classes_Exercises.Raw_Data_04;
+package Defining_Classes_Exercises.Raw_Data_04;
 
 public class Engine {
-
     private int engineSpeed;
     private int enginePower;
 
@@ -13,4 +12,5 @@ public class Engine {
     public int getEnginePower() {
         return enginePower;
     }
+    
 }
