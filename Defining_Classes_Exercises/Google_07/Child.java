eb3e3b3c05b8def466ad0a614a9122a5e4b0543a
Denis Buserski <denis.buserski@gmail.com>
2022-07-22 22:14:28 +0300

    Update Child.java

--- Defining_Classes_Exercises/Google_07/Child.java
+++ Defining_Classes_Exercises/Google_07/Child.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Defining_Classes_Exercises.Google_07;
+package Defining_Classes_Exercises.Google_07;
 
 public class Child {
     private String childName;
@@ -13,4 +13,5 @@ public class Child {
     public String toString() {
         return childName + " " + childBirthday;
     }
+    
 }
