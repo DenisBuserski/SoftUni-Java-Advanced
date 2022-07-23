ade051f67e278a091151d5ba9779b1a0163f53bb
Denis Buserski <denis.buserski@gmail.com>
2022-07-23 22:53:57 +0300

    Update Jar.java

--- Generics_Lab/Jar_Of_T_01/Jar.java
+++ Generics_Lab/Jar_Of_T_01/Jar.java
@@ -1,11 +1,9 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Generics_Lab.Generics_Lab_Jar_Of_T_01;
+package Generics_Lab.Generics_Lab_Jar_Of_T_01;
 
 import java.util.ArrayDeque;
 
 public class Jar<Type> {
-
     private ArrayDeque<Type> stack;
-
 
     public Jar() {
         this.stack = new ArrayDeque<>();
@@ -18,4 +16,5 @@ public class Jar<Type> {
     public Type remove() {
         return this.stack.pop();
     }
+    
 }
