a336251d205bb350efdac903dd5c9bd3cde34062
Denis Buserski <denis.buserski@gmail.com>
2022-07-23 22:55:03 +0300

    Update Main.java

--- Generics_Lab/Generic_Scale_03/Main.java
+++ Generics_Lab/Generic_Scale_03/Main.java
@@ -1,12 +1,11 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Generics_Lab.Generics_Lab_Generic_Scale_03;
+package Generics_Lab.Generics_Lab_Generic_Scale_03;
 
 public class Main {
     public static void main(String[] args) {
 
         Scale<String> scale = new Scale<>("a", "z");
-        //Scale<Integer> scale = new Scale<>(73, 13);
+        // Scale<Integer> scale = new Scale<>(73, 13);
 
         System.out.println(scale.getHeavier());
-
     }
 }
