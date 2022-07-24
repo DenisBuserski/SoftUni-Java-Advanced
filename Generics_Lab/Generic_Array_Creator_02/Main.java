31b7599f8a0cd4675c7026f671c0370d5b419103
Denis Buserski <denis.buserski@gmail.com>
2022-07-24 09:27:36 +0300

    Update Main.java

--- Generics_Lab/Generic_Array_Creator_02/Main.java
+++ Generics_Lab/Generic_Array_Creator_02/Main.java
@@ -1,16 +1,10 @@
-package GenericArray;
-
-import GenericArray.ArrayCreator;
+package Generics_Lab.Generic_Array_Creator_02;
 
 public class Main {
     public static void main(String[] args) {
 
-        String[] javas = ArrayCreator
-                        .<String>create(String.class, 13, "Java");
+        String[] javas = Array_Creator.<String>create(String.class, 13, "Java");
 
-
-        Integer[] integers = ArrayCreator
-                        .<Integer>create(Integer.class, 73, 69);
-
+        Integer[] integers = Array_Creator.<Integer>create(Integer.class, 73, 69);
     }
 }
