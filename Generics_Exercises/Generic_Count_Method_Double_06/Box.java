5b887d1345d0353507efb31017f3d8ba0afe0cc1
Denis Buserski <denis.buserski@gmail.com>
2022-07-27 20:52:22 +0300

    Update Box.java

--- Generics_Exercises/Generic_Count_Method_Double_06/Box.java
+++ Generics_Exercises/Generic_Count_Method_Double_06/Box.java
@@ -1,10 +1,9 @@
-package Box;
+package Generics_Exercises.Generic_Count_Method_Double_06;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Box<T extends Comparable<T> > {
-
     private List<T> elements;
 
     public Box() {
