80592ccab7196dd4ef18f3efd7458db8bb5544ec
Denis Buserski <denis.buserski@gmail.com>
2022-07-27 21:03:51 +0300

    Update Library.java

--- Iterators_And_Comparators_Lab/Library_02/Library.java
+++ Iterators_And_Comparators_Lab/Library_02/Library.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exercises.Iterators_And_Comparators_Lab.Library_02;
+package Iterators_And_Comparators_Lab.Library_02;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -10,12 +10,15 @@ public class Library implements Iterable<Book> {
     public Library() {
         this.books = new ArrayList<>();
     }
+    
     public void add(Book book) {
         this.books.add(book);
     }
+    
     public Book get(int index) {
         return this.books.get(index);
     }
+    
     public int booksCount() {
         return this.books.size();
     }
@@ -43,4 +46,5 @@ public class Library implements Iterable<Book> {
         // return new BookIterator(books);
         return books.iterator();
     }
+    
 }
