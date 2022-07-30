f124f331b59eff4968a1963cea837b1b1233f4ae
Denis Buserski <denis.buserski@gmail.com>
2022-07-30 12:47:37 +0300

    Update Main.java

--- Java_Advanced_Exam_28_June_2020/Parking_03/Main.java
+++ Java_Advanced_Exam_28_June_2020/Parking_03/Main.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exams.Java_Advanced_Exam_28_June_2020.Parking_03;
+package Java_Advanced_Exam_28_June_2020.Parking_03;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,7 +15,7 @@ public class Main {
         // Add Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exams.Java_Advanced_Exam_28_June_2020.Parking_03.Car
         parking.add(volvo);
 
-// Remove Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exams.Java_Advanced_Exam_28_June_2020.Parking_03.Car
+        // Remove Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exams.Java_Advanced_Exam_28_June_2020.Parking_03.Car
         System.out.println(parking.remove("Volvo", "XC90")); // false
         System.out.println(parking.remove("Volvo", "XC70")); // true
 
@@ -25,23 +25,21 @@ public class Main {
         parking.add(peugeot);
         parking.add(audi);
 
-       // Get Latest Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exams.Java_Advanced_Exam_28_June_2020.Parking_03.Car
+        // Get Latest Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exams.Java_Advanced_Exam_28_June_2020.Parking_03.Car
         Car latestCar = parking.getLatestCar();
         System.out.println(latestCar); // Peugeot 307 (2011)
 
-// Get Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exams.Java_Advanced_Exam_28_June_2020.Parking_03.Car
+        // Get Java_Development.Java_Advanced.Java_Advanced_2.Java_Advanced_Exams.Java_Advanced_Exam_28_June_2020.Parking_03.Car
         Car audiS4 = parking.getCar("Audi", "S4");
         System.out.println(audiS4); // Audi S4 (2005)
 
-// Count
+        // Count
         System.out.println(parking.getCount()); // 2
 
-// Get Statistics
+        // Get Statistics
         System.out.println(parking.getStatistics());
-// The cars are parked in Underground parking garage:
-// Peugeot 307 (2011)
-// Audi S4 (2005)
-
-
+        // The cars are parked in Underground parking garage:
+        // Peugeot 307 (2011)
+        // Audi S4 (2005)
     }
 }
